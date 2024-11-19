c21effcd19de916d9076e0ac632804a349a25bff
Luca Lobascio <48275371+ll2909@users.noreply.github.com>
2024-11-19 11:27:18 +0100

    Add files via upload

--- ExplainPipeline.py
+++ ExplainPipeline.py
@@ -1,4 +1,14 @@
-#TODO
+import torch
+from xai.ShapExplainer import explain_beeswarm
+from LiefMLP import load_model
 
 def execute_pipeline(conf):
+    explain_beeswarm(model = load_model(conf["model_path"]),
+                     train_dataset = torch.load(conf["train_tensor_path"]),
+                     bk_size = conf.getint("background_knowledge_size"),
+                     test_dataset = torch.load(conf["test_tensor_path"]),
+                     class_idx = conf.getint("class_index"),
+                     flist_filepath = conf["features_list"],
+                     shap_out_path = conf["expl_save_path"]
+    )
     return
